b2b130218565b326b80f6e778a7b3ef6762dbdb7
sub2pewds12 <leanhkhoi1010@gmail.com>
2025-09-26 01:01:38 +0700

    Initial Commit: Generated Go files for Constants and INTEGER data types

---
+++ e1ap_ies/integer_types.go
@@ -0,0 +1,135 @@
+package e1ap_ies
+
+type Averagingwindow int64
+
+type Bitrate int64
+
+type CellGroupId int64
+
+type DrbId int64
+
+type Extendedpacketdelaybudget int64
+
+type GnbCuCpUeE1apId int64
+
+const (
+	GnbCuCpUeE1apIdMinValue = 0
+	GnbCuCpUeE1apIdMaxValue = 4294967295
+)
+
+type GnbCuUpCapacity int64
+
+const (
+	GnbCuUpCapacityMinValue = 0
+	GnbCuUpCapacityMaxValue = 255
+)
+
+type GnbCuUpId int64
+
+const (
+	GnbCuUpIdMinValue = 0
+	GnbCuUpIdMaxValue = 68719476735
+)
+
+type GnbCuUpUeE1apId int64
+
+const (
+	GnbCuUpUeE1apIdMinValue = 0
+	GnbCuUpUeE1apIdMaxValue = 4294967295
+)
+
+type GnbDuId int64
+
+const (
+	GnbDuIdMinValue = 0
+	GnbDuIdMaxValue = 68719476735
+)
+
+type Hfn int64
+
+const (
+	HfnMinValue = 0
+	HfnMaxValue = 4294967295
+)
+
+type InactivityTimer int64
+
+type M7period int64
+
+type Maxdataburstvolume int64
+
+type Maxpacketlossrate int64
+
+type Networkinstance int64
+
+type PdcpSn int64
+
+const (
+	PdcpSnMinValue = 0
+	PdcpSnMaxValue = 262143
+)
+
+type PduSessionId int64
+
+const (
+	PduSessionIdMinValue = 0
+	PduSessionIdMaxValue = 255
+)
+
+type PerExponent int64
+
+type PerScalar int64
+
+type Ppi int64
+
+type Packetdelaybudget int64
+
+type Periodicity int64
+
+type Prioritylevel int64
+
+const (
+	PrioritylevelMinValue = 0
+	PrioritylevelMaxValue = 15
+)
+
+type Procedurecode int64
+
+const (
+	ProcedurecodeMinValue = 0
+	ProcedurecodeMaxValue = 255
+)
+
+type Protocolextensionid int64
+
+const (
+	ProtocolextensionidMinValue = 0
+	ProtocolextensionidMaxValue = Maxprotocolextensions
+)
+
+type ProtocolieId int64
+
+const (
+	ProtocolieIdMinValue = 0
+	ProtocolieIdMaxValue = Maxprotocolies
+)
+
+type Qci int64
+
+const (
+	QciMinValue = 0
+	QciMaxValue = 255
+)
+
+type QosFlowIdentifier int64
+
+const (
+	QosFlowIdentifierMinValue = 0
+	QosFlowIdentifierMaxValue = 63
+)
+
+type Qosprioritylevel int64
+
+type Subscriberprofileidforrfp int64
+
+type Transactionid int64
